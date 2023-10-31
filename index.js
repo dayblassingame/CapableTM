a56e50f781fa1617db6bc012729bbcb4e72236eb
Day Blassingame <dayblassingame@gmail.com>
2023-10-31 19:41:14 -0400

    added data structures

--- index.js
+++ index.js
@@ -5,4 +5,37 @@ import ReactDOM from "react-dom";
 import {App} from './src/App'
 const rootElement = document.getElementById('app');
 
-ReactDOM.render(<App/>, rootElement);
+let tasks = [
+    {
+        name:'walk dog',
+        details:'stay in neightborhood',
+        currentStage:0,
+        dueDate: new Date()
+    },
+    {
+        name:'clean house',
+        details:'fridge, bathroom, and den',
+        currentStage:0,
+        dueDate: new Date()
+    },
+    {
+        name:'cook dinner',
+        details:'asian cuisine',
+        currentStage:0,
+        dueDate: new Date()
+    },
+    {
+        name:'cook dinner',
+        details:'indian cuisine',
+        currentStage:3,
+        dueDate: new Date()
+    },
+    {
+        name:'cook dinner',
+        details:'african cuisine',
+        currentStage:0,
+        dueDate: new Date()
+    }
+]
+
+ReactDOM.render(<App taskProp={tasks}/>, rootElement);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.scss'
 
-export function App(props){
+export function App({taskProp}){
+    let id = 0;
+    const stages = [
+        {
+            name: 'To-do',
+            accordion: true,
+        },
+        {
+            name: 'In Progress',
+            accordion: true,
+        },
+        {
+            name: 'Under Review',
+            accordion: true,
+        },
+        {
+            name: 'Done',
+            accordion: true,
+        }
+    ]
+
+    let taskList = [];
+
+    stages.forEach((stage) =>{
+        taskList.push([]);
+    })
+
+    taskProp.forEach((task) =>{
+        taskList[task.currentStage].push(task);
+    })
+    const [tasks, setTasks] = useState([taskList]);
+
 
     return(
-        <h1>Hello World</h1>
+        <main id='main' className='ctm-main-dashboard'>
+            <span className="ctm-newTaskContainer">
+                <button id='newTask'> Add New Task</button>
+            </span>
+            <span >
+
+            </span>
+            <span className="ctm-tasklistsContainer">
+                 <ul>
+                    {
+                        stages.map((stage)=>{
+                            
+                        })
+                    }
+                 </ul>
+            </span>
+
+        </main>
     )
 }
