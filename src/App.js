3200533a45a91b6b9071ece18364489d922fe5fe
Day Blassingame <dayblassingame@gmail.com>
2023-11-03 09:38:00 -0400

    mapped task array into task components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import './App.scss'
+import Task from "./Task";
 
 export function App({taskProp}){
     let id = 0;
@@ -22,17 +23,13 @@ export function App({taskProp}){
         }
     ]
 
-    let taskList = [];
-
-    stages.forEach((stage) =>{
-        taskList.push([]);
-    })
-
+    let taskList = []
     taskProp.forEach((task) =>{
-        taskList[task.currentStage].push(task);
+        task.id = id;
+        taskList = [...taskList, task];
+        id++;
     })
-    const [tasks, setTasks] = useState([taskList]);
-
+    const [tasks, setTasks] = useState(taskList);
 
     return(
         <main id='main' className='ctm-main-dashboard'>
@@ -43,13 +40,33 @@ export function App({taskProp}){
 
             </span>
             <span className="ctm-tasklistsContainer">
-                 <ul>
-                    {
-                        stages.map((stage)=>{
-                            
-                        })
+                    {stages.map((stage, index) => {
+                        return(
+                            <ul key={stage+index}>
+                                <label>{stage.name}</label>
+                                <span id={stage.name +'li'} >
+                                    {
+                                        tasks.map((task) =>{
+                                            if(task.currentStage !== index)
+                                                return;
+                                            return(
+                                                <Task
+                                                    key={task.id}
+                                                    id={task.id}
+                                                    name={task.name}
+                                                    details={task.details}
+                                                    date={task.dueDate}
+                                                    stage={task.currentStage}
+                                                 />
+                                            )
+                                        })
+                                    }
+                                </span>
+                            </ul>
+                        )
+                    })
                     }
-                 </ul>
+
             </span>
 
         </main>
---
+++ src/Task.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Task(props){
+    const id = props.id;
+    const name = props.name;
+    const details = props.details;
+    let date = props.date;
+    let stage = props.stage;
+
+    const formatDate = (date) =>{
+        const month= date.getMonth();
+        const day = date.getDay();
+        const year = date.getFullYear();
+
+        return(month + "/" + day + "/" + year.toString().slice(2));
+    }
+    return(
+        <li key={id}> 
+            <span>
+                <label>{name}</label>
+                <button id='delete'>X</button>
+                <p>{details}</p>
+            </span>
+            <span>
+                <p>{formatDate(date)}</p>
+                <button id='prev'>{'<<'}</button>
+                <button id='next'>{'>>'}</button>
+            </span>
+        </li>
+    );
+}
