d1a0accf2913777bd820d53e7b9689c27efdbf5b
Day Blassingame <dayblassingame@gmail.com>
2023-11-13 00:15:28 -0500

    removed test data

--- index.js
+++ index.js
@@ -5,37 +5,5 @@ import ReactDOM from "react-dom";
 import {App} from './src/App'
 const rootElement = document.getElementById('app');
 
-let tasks = [
-    {
-        name:'walk dog',
-        details:'stay in neightborhood',
-        currentStage:0,
-        dueDate: new Date()
-    },
-    {
-        name:'clean house',
-        details:'fridge, bathroom, and den',
-        currentStage:0,
-        dueDate: new Date()
-    },
-    {
-        name:'cook dinner',
-        details:'asian cuisine',
-        currentStage:0,
-        dueDate: new Date()
-    },
-    {
-        name:'cook dinner',
-        details:'indian cuisine',
-        currentStage:3,
-        dueDate: new Date()
-    },
-    {
-        name:'cook dinner',
-        details:'african cuisine',
-        currentStage:0,
-        dueDate: new Date()
-    }
-]
 
-ReactDOM.render(<App taskProp={tasks}/>, rootElement);
+ReactDOM.render(<App/>, rootElement);
